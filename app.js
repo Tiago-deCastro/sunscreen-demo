1391f219aaac93cd6fa9b5ecc0e74ccfa018ca23
TiagodeCastro <tiagodecastro.sf@gmail.com>
2022-04-27 10:21:08 +0100

    Hamburger menu implementation and page resize

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
+/* dropdown menu */
 const productsBtn = document.querySelector(".products");
 const ddMenuul = document.querySelector(".dd_menu_ul");
 const arrow = document.querySelector(".fa-caret-down");
 
+/* burger menu */
+
+const hamburgerBtn = document.querySelector(".hamburger");
+const navbarMenu = document.querySelector(".navbar-menu");
+
 productsBtn.addEventListener("click", function () {
   ddMenuul.classList.toggle("active_dd");
   arrow.classList.toggle("rotateArrow");
+});
+
+hamburgerBtn.addEventListener("click", function () {
+  navbarMenu.classList.toggle("open");
 });
